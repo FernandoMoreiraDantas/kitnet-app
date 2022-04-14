4135fccb4444b9b29ba5191ab9827b26a26512c2
fernando.moreiradantas@gmail.com <fernando.moreiradantas@gmail.com>
2022-04-14 19:47:31 -0300

    Url de produção

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const httpCliente = axios.create(
     {   
-        baseURL:'http://127.0.0.1:8080',withCredentials:true
+        baseURL:'https://kitnet-api.herokuapp.com',withCredentials:true
     }
 
 )
